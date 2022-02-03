e51b3584186aa0e4ca020ebff4be6e1b7791adf0
vigneshraaj19 <94341719+vigneshraaj19@users.noreply.github.com>
2022-02-03 14:08:57 +0530

    movie listing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import "./App.css";
 import React, { useState } from 'react';
 
+
 export default function App() {
   const users=[{names:"The Avengers",
   profile:"https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
-rates:"⭐⭐⭐⭐⭐",directs:"Joseph Eggleston Johnston",summarys:"Marvel's The Avengers (classified under the name Marvel Avengers Assemble in the United Kingdom and Ireland), or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the same name."},
+rates:"8",directs:"Joseph Eggleston Johnston",summarys:"Marvel's The Avengers (classified under the name Marvel Avengers Assemble in the United Kingdom and Ireland), or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the same name."},
 {names:"Interstellar",
   profile:"https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
-rates:"⭐⭐⭐⭐⭐",directs:"Joseph Eggleston Johnston",summarys:"Marvel's The Avengers (classified under the name Marvel Avengers Assemble in the United Kingdom and Ireland), or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the same name."},
+rates:"9",directs:"Joseph Eggleston Johnston",summarys:"Marvel's The Avengers (classified under the name Marvel Avengers Assemble in the United Kingdom and Ireland), or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the same name."},
 {names:"Jai Bhim",
   profile:"https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
-rates:"⭐⭐⭐⭐⭐",directs:"Joseph Eggleston Johnston",summarys:"Marvel's The Avengers (classified under the name Marvel Avengers Assemble in the United Kingdom and Ireland), or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the same name."},
+rates:"8.5",directs:"Joseph Eggleston Johnston",summarys:"Marvel's The Avengers (classified under the name Marvel Avengers Assemble in the United Kingdom and Ireland), or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the same name."},
 {names:"Baahubali",
 profile:"https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
-rates:"⭐⭐⭐⭐⭐",directs:"Joseph Eggleston Johnston",summarys:"Marvel's The Avengers (classified under the name Marvel Avengers Assemble in the United Kingdom and Ireland), or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the same name."}
+rates:"8.1",directs:"Joseph Eggleston Johnston",summarys:"Marvel's The Avengers (classified under the name Marvel Avengers Assemble in the United Kingdom and Ireland), or simply The Avengers, is a 2012 American superhero film based on the Marvel Comics superhero team of the same name."}
  ];
-
+ 
     return (
     <div className="App">
-     <h6>🍿🍿🍿WELCOME TO SATHYAM CINEMAS🍿🍿🍿</h6>
-     <h6>MOVIE BUFF📽️🎬📽️🎦</h6>
-     <h6>DIWALI RELEASED MOVIES THIS WEEK📽️🍿🎬🎦👍1</h6>
-
-  {users.map(nm=> <Welcome name={nm.names} logo={nm.profile} rate={nm.rates} direct={nm.directs} summary={nm.summarys}/>)}
-  
- 
+      <div className="movie-list">
+        {users.map(({names,profile,rates,summarys})=>
+        (<Movie
+        names={names}
+        profile={profile}
+        rates={rates}
+        summarys={summarys}
+        />
+        ))}
+<Addbox />
+<Colorbox />
+        
+      </div>
     </div>
   );
 }
@@ -47,19 +54,63 @@ function Counters()
     </div>
   );
 }
-function Welcome({name,logo,rate,direct,summary})
+
+function Movie({names,profile,rates,summarys})
 {
+ const styles={color:rates>8 ? "green" : "red",};
   return(
-    <div className="Welcome">
-      <img className="userinput"
-      src={logo} alt="Logo" />;
-       <h1>{name}</h1>
-       <h3>Ratings:{rate}</h3>
-       <h3>Directed by:{direct}</h3>
-       <h3>Summary:</h3><span>{summary}</span>
+    <div className="movie-container">
+      <img className="movie-poster"
+      src={profile} alt="Logo" />;
+      <div className="movie-specs">
+       <h1 className="movie-name">{names}</h1>
+       <h3 style={styles} className="movie-rating">⭐{rates}</h3>
+       </div>
+       <div className="movie-summary">{summarys}</div>
+       <div className="same">
        <Counter /> 
        <Counters />
-      
+       </div>
       </div>
   );
+}
+
+function Addbox()
+{
+  const [color, setColor] = useState("pink");
+  const styles={
+    background:color,
+  };
+  const[colorList,setColorList] = useState([
+    "green",
+    "orange",
+    "blue",
+    "red"
+  ]);
+  return(
+    <div>
+      <input
+      value={color}
+      style={styles}
+      onChange={(event)=>setColor(event.target.value)}
+      placeholder="Enter the value"
+      />
+      <button onClick={()=>setColorList([...colorList,color])}>Add color </button>
+      {colorList.map((clr)=>(
+        <Colorbox color={clr} />
+      ))
+      }
+    </div>
+  )
+}
+
+function Colorbox({ color})
+{
+  const styles = {
+    backgroundColor:color,
+    height:"25px",
+    width:"200px",
+    marginTop:"10px",
+  };
+  return <div style={styles}></div>
 }
